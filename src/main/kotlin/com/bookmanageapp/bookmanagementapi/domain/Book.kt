25d01b4cf411daf46418e5ab72a7865610cd7bf8
unknown <21nose.jp@gmail.com>
2025-09-22 14:59:32 +0900

    レビュー事項対応

--- src/main/kotlin/com/bookmanageapp/bookmanagementapi/domain/Book.kt
+++ src/main/kotlin/com/bookmanageapp/bookmanagementapi/domain/Book.kt
@@ -11,15 +11,6 @@ data class Book(
     val authorIds: List<Long>,
     val lockNo: Int = 1,
 ) {
-    init {
-        require(title.isNotBlank()) { "Title cannot be blank" }
-        require(price >= BigDecimal.ZERO) { "Price must be greater than or equal to 0" }
-        require(currencyCode.isNotBlank()) { "Currency code cannot be blank" }
-        require(currencyCode.length == 3) { "Currency code must be 3 characters" }
-        require(authorIds.isNotEmpty()) { "Book must have at least one author" }
-        require(lockNo > 0) { "Lock number must be positive" }
-    }
-
     fun canUpdatePublicationStatus(newStatus: PublicationStatus): Boolean {
         return publicationStatus.canTransitionTo(newStatus)
     }
