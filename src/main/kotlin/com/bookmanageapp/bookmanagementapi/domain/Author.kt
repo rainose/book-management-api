b7b000a37985936a66200347ee6a8e8b5f0aad89
unknown <21nose.jp@gmail.com>
2025-09-23 09:58:33 +0900

    nullableの扱いを修正

--- src/main/kotlin/com/bookmanageapp/bookmanagementapi/domain/Author.kt
+++ src/main/kotlin/com/bookmanageapp/bookmanagementapi/domain/Author.kt
@@ -2,8 +2,16 @@ package com.bookmanageapp.bookmanagementapi.domain
 
 import java.time.LocalDate
 
+// 新規作成用（IDなし）
+data class NewAuthor(
+    val name: String,
+    val birthDate: LocalDate,
+    val lockNo: Int = 1,
+)
+
+// 永続化済み（IDあり）
 data class Author(
-    val id: Long? = null,
+    val id: Long,
     val name: String,
     val birthDate: LocalDate,
     val lockNo: Int = 1,
--- src/main/kotlin/com/bookmanageapp/bookmanagementapi/repository/AuthorRepository.kt
+++ src/main/kotlin/com/bookmanageapp/bookmanagementapi/repository/AuthorRepository.kt
@@ -1,9 +1,10 @@
 package com.bookmanageapp.bookmanagementapi.repository
 
 import com.bookmanageapp.bookmanagementapi.domain.Author
+import com.bookmanageapp.bookmanagementapi.domain.NewAuthor
 
 interface AuthorRepository {
-    fun create(author: Author): Long?
+    fun create(author: NewAuthor): Long?
 
     fun update(author: Author)
 
